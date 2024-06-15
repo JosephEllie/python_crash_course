278c4acca863c51228ec8dd4875c2a59302a0e1a
JosephEllie <josephelliecooper@gmail.com>
2024-06-15 02:27:02 +0000

    exercise 5 added

---
+++ 5.py
@@ -0,0 +1,48 @@
+# -------------exercise 5-8-----------
+
+# usernames = ["admin", "joe", "jane", "jim", "jack"]
+
+# for username in usernames:
+#     if username == "admin":
+#         print("Hello admin, would you like to see a           status report?")
+#     else:
+#         print(f"Hello {username}, thank you for logging       in again.")
+
+# --------exercise 5-9-------------
+
+# usernames = []
+
+# if not usernames:
+#     print("We need to find some users!")
+
+# --------exercise 5-10-------------
+# current_users = ["tom", "ben", "joe", "sam", "dan"]
+# new_users = ["jane", "mic", "joe", "ben", "rita"]
+
+# for new_user in new_users:
+#     if new_user in current_users:
+#         print(f"The username {new_user.title()} is not available")
+#     else:
+#         print(f"The username {new_user.title()} is available")
+# print("Please choose another username")
+
+# name = "ben"
+# another_name = "Ben"
+
+# print(name == another_name)
+
+# -----------5-11----------------
+
+# ordinal_values = []
+# for value in range(1, 10):
+#     ordinal_values.append(value)
+
+# for ordinal_value in ordinal_values:
+#     if ordinal_value == 1:
+#         print(f"{ordinal_value}st")
+#     elif ordinal_value == 2:
+#         print(f"{ordinal_value}nd")
+#     elif ordinal_value == 3:
+#         print(f"{ordinal_value}rd")
+#     elif ordinal_value:
+#         print(f"{ordinal_value}th")
